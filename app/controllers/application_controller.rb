2f2b347a939f24ab4cc3399f98c5505b9136f676
Tien Ta <taminhtien1993@gmail.com>
2015-05-10 07:34:35 +0000

    Add goodbye action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   def hello
-    render text: "Hello World!"
+    render text: "¡Hola, mundo!"
+  end
+  def goodbye
+    render text: "Goodbye, World!"
   end
 end
